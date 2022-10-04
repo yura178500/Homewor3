faba541b0a20f637bdd49cb4df6ab94a2936d1f7
yura178500 <yura178500@yandex.ru>
2022-10-04 20:20:41 +0300

    Задача 1-8

--- src/Main.java
+++ src/Main.java
@@ -61,5 +61,74 @@ public class Main {
         System.out.println(students);
 
         //Задача 5
+        int cansAll = 120;
+        System.out.println(cansAll);
+        int white = 2;
+        System.out.println(white);
+        int brown = 4;
+        System.out.println(brown);
+        int class = white + brown;
+        System.out.println(class);
+        int classAll = cansAll / class;
+        System.out.println(classAll);
+        int cansWhite = classAll * white;
+        System.out.println(cansWhite);
+        int cansBrown = classAll * brown;
+        System.out.println(cansBrown);
+
+        //Задача 6
+        int banana = 80;
+        int bananaAll = 5;
+        int result1 = 80 * 5;
+        System.out.println("banana * bananaAll = "+ result1);
+        int milk = 105;
+        int milkAll = 2;
+        int result2 = 105 * 2;
+        System.out.println("milk * milkAll = "+ result2);
+        int iceCream = 100;
+        int iceAll = 2;
+        int result3 = 100 * 2;
+        System.out.println("iceCream * iceAll = "+result3);
+        int eggs = 70;
+        int eggsAll = 4;
+        int result4 = 70 * 4;
+        System.out.println("eggs * eggsAll = "+result4);
+        int foodAll = result1 + result2 + result3+ result4;
+        System.out.println(foodAll);
+
+        //Задача 7
+        int weight = 7000;
+        System.out.println(weight);
+        int day1 = 250;
+        System.out.println(day1);
+        int dayAll1 = weight / day1;
+        System.out.println(dayAll1);
+        int day2 = 500;
+        System.out.println(day2);
+        int dayAll2 = weight / day2;
+        System.out.println(dayAll2);
+
+        //Задача 8
+        int Macha = 67760;
+        System.out.println(Macha);
+        int income1 = Macha * 10%;
+        System.out.println(income1);
+        int Denis = 83690;
+        System.out.println(Denis);
+        int income2 = Denis * 10%;
+        System.out.println(income2);
+        int Cristi = 76230;
+        System.out.println(Cristi);
+        int income3 = Cristi * 10%;
+        System.out.println(income3);
+        int incomeAll = Macha + Denis + Cristi;
+        System.out.println(incomeAll);
+        int incomeAll% = income1 + income2 + income3;
+        System.out.println(incomeAll%);
+        int difference = incomeAll% - incomeAll;
+        System.out.println(difference);
+
+
+
 
 }
