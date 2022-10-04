4f1afc87dc94383ae482ebe5e5b6ae5cbae3a957
yura178500 <yura178500@yandex.ru>
2022-10-04 18:15:21 +0300

    Задача 1-8

---
+++ src/Main.java
@@ -0,0 +1,65 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1
+        int a = 7;
+        System.out.println(a);
+        byte b = 5;
+        System.out.println(b);
+        short s = 100;
+        System.out.println(s);
+        long r = 80;
+        System.out.println(r);
+        float f = 45.78f;
+        System.out.println(f);
+        double d = 68.42;
+        System.out.println(d);
+
+        //Задача 2
+        int a = 27897;
+        System.out.println(a);
+        byte b = 67;
+        System.out.println(b);
+        short s = -159;
+        System.out.println(s);
+        long r = 987 678 965 549;
+        System.out.println(r);
+        float f = 2,786;
+        System.out.println(f);
+        double d = 27.12;
+        System.out.println(d);
+        boolean isAdult =  false
+        System.out.println(isAdult);
+        char y = 569;
+        System.out.println(y);
+
+        //Задача 3
+        int teacher1 = 23;
+        System.out.println(teacher1);
+        int teacher2 = 27;
+        System.out.println(teacher2);
+        int teacher3 = 30;
+        System.out.println(teacher3);
+        int students = teacher1 + teacher2 + teacher3;
+        System.out.println(students);
+        int paper = 480;
+        System.out.println(paper);
+        int StudentsPaper = paper / students;
+        System.out.println(StudentsPaper);
+
+        //Задача 4
+        int machine = 16;
+        System.out.println(machine);
+        int BattleMin = machine / 2;
+        System.out.println(BattleMin);
+        int battle20 = BattleMin * 20;
+        System.out.println(students);
+        int battleSyt = BattleMin * 1440;
+        System.out.println(students);
+        int battle3Syt = BattleMin * 4320;
+        System.out.println(students);
+        int battle1M = BattleMin * 17280;
+        System.out.println(students);
+
+        //Задача 5
+
+}
